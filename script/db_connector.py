37f1d33ed16b7474ae5889603b087c0872e2f753
eel55114 <eel55114@gmail.com>
2026-03-26 16:34:58 +0900

    DB 초기화 기능 추가

--- script/db_connector.py
+++ script/db_connector.py
@@ -14,6 +14,24 @@ class DBConnector:
         )
         self.cur = self.conn.cursor()
 
+        self.initialize()
+
+    def initialize(self):
+
+        try:
+            self.cur.execute((
+                "create table if not exists lidardata("
+                    "id int primary key auto_increment,"
+                    "ranges json,"
+                    "when_ datetime,"
+                    "action varchar(50)"
+                ");"
+            ))
+            self.conn.commit()
+        except:
+            self.conn.rollback()
+
+
     def put_data(self, ranges:list[float], when:datetime, action:str):
         try:
             json_ranges = json.dumps(ranges)
@@ -21,7 +39,6 @@ class DBConnector:
                 '''insert into lidardata(ranges, when_, action) values(%s,%s,%s)''',
                 (json_ranges,when,action)
             )
-            self.cur.fetchall()
             self.conn.commit()
         except:
             self.conn.rollback()
@@ -33,3 +50,18 @@ if __name__ == '__main__':
 
     db = DBConnector(HOST, PW)
     cursor = db.conn.cursor()
+
+    #
+    # import time
+    # import random
+    #
+    # for i in range(100):
+    #     lidar = [250-(random.random()*30) for _ in range(360)]
+    #     action = random.sample(["move_front", "turn_left", "turn_right"], 1)[0]
+    #     db.put_data(
+    #         lidar,
+    #         datetime.now(),
+    #         action
+    #     )
+    #
+    #     time.sleep(0.5)
