027aeba1c7ce17a080bc797eb24719afeb60b286
eel55114 <eel55114@gmail.com>
2026-03-26 17:03:33 +0900

    DB 데이터 조회 및 Pandas Dataframe으로 출력 기능 추가

--- script/db_connector.py
+++ script/db_connector.py
@@ -2,6 +2,7 @@ import json
 
 import pymysql
 from datetime import datetime
+import pandas as pd
 
 class DBConnector:
     def __init__(self, host, pw):
@@ -43,6 +44,35 @@ class DBConnector:
         except:
             self.conn.rollback()
 
+    def get_data(self, from_n:int|None=None, to_n:int|None=None, latest_n:int|None=None, return_pd:bool=False):
+        if (from_n or to_n) and latest_n:
+            raise Exception("ID range selector(from_n, to_n) is not compatible with latest selector(latest_n)")
+        elif (from_n is None) != (to_n is None):
+            raise Exception(f"the value needed: {'to_n' if to_n is None else 'from_n'}")
+
+
+        stmt = "select action, ranges from lidardata "
+        args = ()
+        if latest_n is not None:
+            stmt += "order by id desc limit %s"
+            args = (latest_n,)
+        elif from_n is not None:
+            stmt += "where id between %s and %s"
+            args = (from_n, to_n)
+
+        stmt += ";"
+
+        if return_pd:
+            return pd.read_sql_query(stmt, self.conn, params=list(args))
+        else:
+            try:
+                self.cur.execute(stmt, args)
+                return self.cur.fetchall()
+            except Exception as e:
+                self.conn.rollback()
+                raise e
+
+
 
 if __name__ == '__main__':
     HOST = 'localhost'
@@ -51,6 +81,8 @@ if __name__ == '__main__':
     db = DBConnector(HOST, PW)
     cursor = db.conn.cursor()
 
+    # df = db.get_data(return_pd=True, from_n=20, to_n=50)
+    # print(df)
     #
     # import time
     # import random
